d811b0f0be60eaa50fa911c9c094c6f4d5f8e93a
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-18 14:19:35 +0700

    [Bài tập] Tìm ước số chung

--- UCLN.java
+++ UCLN.java
@@ -4,19 +4,19 @@ public class UCLN {
         int b;
         java.util.Scanner sc = new java.util.Scanner(System.in);
         System.out.println("Enter a:");
-        a=sc.nextInt();
+        a = sc.nextInt();
         System.out.println("Enter b:");
-        b=sc.nextInt();
+        b = sc.nextInt();
         // ham tra ve gia tri tuyet doi
         a = Math.abs(a);
         b = Math.abs(b);
-        if(a==0 || b==0)
+        if (a == 0 || b == 0)
             System.out.println("No greatest common factor:");
-        while (a!=b){
-            if(a>b)
-                a=a-b;
+        while (a != b) {
+            if (a > b)
+                a = a - b;
             else
-                b=b-a;
+                b = b - a;
         }
         System.out.println("Greatest common factor: " + a);
     }
